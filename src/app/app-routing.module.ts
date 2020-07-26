ac665386158862092887af3d205eca3130c9ce0c
randerson <randerson_d@hotmail.com>
2020-07-26 04:20:32 -0300

    class of service to access the routes of the listing endpoint of all contacts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { ContactEditComponent } from './contact-edit/contact-edit.component';
 
 
-
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path:'',
+		component: ContactComponent
+	},
+	{
+		path:'contact/edit/:id',
+		component: ContactEditComponent
+	}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/services/data.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private httpClient:HttpClient) { }
+
+
+  getData(){
+    return this.httpClient.get('http://127.0.0.1:5000/api/v1/contacts/');
+  }
+
+}
